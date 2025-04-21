9d424c8fa20a823b6095dd37e75dd7caebde7eb5
lautaromdelgado <lautaromdelgado@gmail.com>
2025-04-21 12:15:28 -0300

    Comentarios y nueva ruta para eliminar usuarios por id solo si eres admin

--- delivery/http/router/routes.go
+++ delivery/http/router/routes.go
@@ -8,14 +8,25 @@ import (
 
 func InitRoutes(e *echo.Echo, authHandler *handler.AuthHandler, secret string) {
 	// Rutas públicas
+	// Ruta: /api
 	public := e.Group("/api")
 	public.POST("/register", authHandler.Register)
 	public.POST("/login", authHandler.Login)
 
 	// Rutas privadas (requieren JWT)
+	// Ruta: /api
 	private := e.Group("/api")
 	private.Use(middleware.JWTMiddleware(secret))
+
+	// Rutas privadas para administradores
+	// Ruta: /api/admin/
+	admin := private.Group("/admin")
+	admin.Use(middleware.OnlyAdmin()) // Middleware para verificar si el usuario es admin
+
 	// USUARIOS
 	private.PUT("/update/user", authHandler.Update)    // Actualizar usuario por ID
 	private.DELETE("/delete/user", authHandler.Delete) // Eliminar usuario por ID
+
+	// ADMINISTRADORES
+	admin.DELETE("/delete/user/:id", authHandler.DeleteByID) // Eliminar usuario por ID siendo admin
 }
