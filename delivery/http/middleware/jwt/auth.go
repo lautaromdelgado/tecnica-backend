7f279a681684c6cc0d58d28a0e4efa234f1ebc02
lautaromdelgado <lautaromdelgado@gmail.com>
2025-04-21 12:15:51 -0300

    feat: middleware para verificar que lapetición se haga solo si es admin

---
+++ delivery/http/middleware/jwt/auth.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/lautaromdelgado/tecnica-backend/delivery/http/middleware"
+)
+
+// OnlyAdmin es un middleware que verifica si el usuario tiene el rol de administrador
+// y permite el acceso a la ruta solo si es así. Si no, devuelve un error 403 Forbidden.
+func OnlyAdmin() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			_, role, err := middleware.GetUserFromContext(c)
+			if err != nil || role != "admin" {
+				return echo.NewHTTPError(403, "Admin only")
+			}
+			return next(c)
+		}
+	}
+}
